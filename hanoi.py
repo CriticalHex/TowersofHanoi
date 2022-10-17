230206613df4b7775d2f95a8bbfb8c1965bc9ad0
CriticalHex <80023875+CriticalHex@users.noreply.github.com>
2022-10-17 16:43:02 -0600

    scopes solved, setup refactored, and snapping done

--- hanoi.py
+++ hanoi.py
@@ -3,10 +3,6 @@ import random
 
 pygame.init()
 
-BLUE = (0, 0, 200)
-PINK = (200, 0, 200)
-RED = (200, 0, 0)
-GREEN = (0, 200, 0)
 WHITE = (255, 255, 255)
 
 
@@ -20,13 +16,15 @@ class Tower:
 
 
 class Disk:
-    def __init__(
-        self, value: int, color: tuple[int, int, int], pos: pygame.Vector2, width: int
-    ) -> None:
-        self.start_pos = pos
-        self.color = color
+    def __init__(self, value: int, rect: pygame.Rect) -> None:
+        self.color = (
+            random.randint(0, 255),
+            random.randint(0, 255),
+            random.randint(0, 255),
+        )
         self.value = value
-        self.rect = pygame.rect.Rect(*self.start_pos, width, 50)
+        self.rect = rect
+        self.last = self.rect.center
 
     def draw(self, screen):
         pygame.draw.rect(screen, self.color, self.rect)
@@ -37,11 +35,6 @@ def get_hovering(mouse_pos: pygame.Vector2, towers: list[Tower]):
         for d in t.disks:
             if d.rect.collidepoint(mouse_pos):
                 return d, t
-    return None
-
-
-def rc():
-    return random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
 
 
 def highlight(screen: pygame.Surface, rect: pygame.Rect):
@@ -49,30 +42,20 @@ def highlight(screen: pygame.Surface, rect: pygame.Rect):
 
 
 def reset(screen: pygame.Surface, disk_count: int):
+    center = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+    y = screen.get_height() - 600
     towers: list[Tower] = []
-    towers.append(Tower(pygame.Vector2(center.x - 500, screen.get_height() - 600)))
-    towers.append(Tower(pygame.Vector2(center.x - 50, screen.get_height() - 600)))
-    towers.append(Tower(pygame.Vector2(center.x + 450, screen.get_height() - 600)))
+    towers.append(Tower(pygame.Vector2(center.x - 500, y)))
+    towers.append(Tower(pygame.Vector2(center.x - 50, y)))
+    towers.append(Tower(pygame.Vector2(center.x + 450, y)))
 
     for i in range(disk_count):
-        t1 = towers[0]
-        pos_scale = (i + 1) * 50
-        size_scale = abs((i - (disk_count + 1)) * 50)
-        t1.disks.append(
-            Disk(
-                i,
-                rc(),
-                pygame.Vector2(
-                    (t1.rect.x + (t1.rect.w / 2)) - (size_scale / 2),
-                    screen.get_height() - pos_scale,
-                ),
-                size_scale,
-            )
-        )
+        scale = abs((i - (disk_count + 1)) * 50)
+        set_disk(Disk(i, pygame.Rect(0, 0, scale, 50)), towers[0])
     return towers
 
 
-def get_tower(edit: Disk, mouse_pos: pygame.Vector2, towers: list[Tower]):
+def get_tower(edit: Disk, towers: list[Tower]):
     for t in towers:
         if edit.rect.colliderect(t.rect):
             return t
@@ -83,24 +66,24 @@ def grab_disk(disk: None | Disk, tower: None | Tower):
         return disk
 
 
-def stat_rect(screen: pygame.Surface, color: tuple[int, int, int]):
-    pygame.draw.rect(screen, color, (0, 0, 50, 50))
-
-
-def set_disk(edit: Disk, start_tower: Tower, tower: Tower):
-    start_tower.disks.remove(edit)
+def set_disk(edit: Disk, tower: Tower, start_tower: None | Tower = None):
+    if start_tower:
+        start_tower.disks.remove(edit)
+    edit.rect.center = tower.rect.center
+    if tower.disks:
+        edit.rect.bottom = tower.disks[-1].rect.top
+    else:
+        edit.rect.bottom = tower.rect.bottom
     tower.disks.append(edit)
-    edit.start_pos = edit.rect.topleft
+    edit.last = edit.rect.center
 
 
-if __name__ == "__main__":
+def main():
     screen = pygame.display.set_mode((1920, 1080))
     pygame.display.set_caption("Towers of Hanoi")
     running = True
-    center = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
     edit: Disk = None
-    # tower: Tower = None
 
     disk_count = 10
 
@@ -126,15 +109,17 @@ if __name__ == "__main__":
                 edit = grab_disk(disk, start_tower)
 
             if event.type == pygame.MOUSEBUTTONUP and edit is not None:
-                tower = get_tower(edit, mouse_pos, towers)
+                tower = get_tower(edit, towers)
                 if tower is not None:
                     if (
                         len(tower.disks) == 0
                         or edit.value > max(tower.disks, key=lambda x: x.value).value
                     ):
-                        set_disk(edit, start_tower, tower)
+                        set_disk(edit, tower, start_tower)
                     else:
-                        edit.rect.topleft = edit.start_pos
+                        edit.rect.center = edit.last
+                else:
+                    edit.rect.center = edit.last
                 edit = None
 
         if edit is not None:
@@ -152,3 +137,7 @@ if __name__ == "__main__":
 
         pygame.display.flip()
     pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
